9633bf597e8987e750173c94c83b5994adb2e6a3
arman kumar <armank8000@gmail.com>
2025-01-14 20:04:44 +0530

    namespace added

--- src/config/config.inc.php
+++ src/config/config.inc.php
@@ -1,5 +1,8 @@
 <?php
+namespace ph7\LearnPHP\config;
+use Dotenv\Dotenv;
+
 $path = dirname((__DIR__),2);
-$dotenv = Dotenv\Dotenv::createImmutable($path);
+$dotenv = Dotenv::createImmutable($path);
 $dotenv->load();
 $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
--- src/config/database.inc.php
+++ src/config/database.inc.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace ph7\LearnPHP\config;
 use RedBeanPHP\R;
 
 $dsn = sprintf("mysql:host=%s;dbname=%s", $_ENV['DB_HOST'], $_ENV['DB_NAME']);
