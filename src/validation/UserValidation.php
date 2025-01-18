1d3ef809497f675bf4a8620ae3d28da617943ff7
Arman kumar <140732286+armank8000@users.noreply.github.com>
2025-01-18 11:14:34 +0530

    Updates (#3)
    * added option for removing user via sending uuid through payload and retrieve all users also
    
    * added option to update data in database

--- src/validation/UserValidation.php
+++ src/validation/UserValidation.php
@@ -21,16 +21,19 @@ public function isCreationSchemaValid(): bool{
 
     $schemeValidation = v::attribute('first', v::stringType()->length(self::minimumNameLength, self::maximumNameLength))
         ->attribute('last', v::stringType()->length(self::minimumNameLength  , self::maximumNameLength))
-        ->attribute('email', v::email(),mandatory: false)
+        ->attribute('email', v::email())
         ->attribute('phone', v::phone(),mandatory: false);
     return $schemeValidation->validate($this->data);
 
 }
 
     public function isUpdateSchemaValid(): bool{
-
-        return $this->isCreationSchemaValid();
-
+    $schemaValidation =
+     v::attribute('user_uuid',v::uuid(version:4))
+     ->attribute('first', v::stringType()->length(self::minimumNameLength, self::maximumNameLength),false)
+          ->attribute('last', v::stringType()->length(self::minimumNameLength  , self::maximumNameLength),false)
+          ->attribute('phone', v::phone(),mandatory: false);
+return $schemaValidation->validate($this->data);
     }
 
     public function isRemoveSchemaValid(): bool{
