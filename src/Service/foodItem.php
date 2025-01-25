ad0399a976c69e7393aedf2fab5a442023bbe472
arman kumar <armank8000@gmail.com>
2025-01-25 21:38:00 +0530

    added webpage

--- src/Service/foodItem.php
+++ src/Service/foodItem.php
@@ -5,8 +5,10 @@ namespace PH7\Learnphp\Service;
 use PH7\JustHttp\StatusCode;
 
 use PH7\Learnphp\DAL\foodItemDal;
+use PH7\Learnphp\Entity\Item as ItemEntity;
 use PH7\Learnphp\validation\exception\InvalidValidationException;
 use PH7\PhpHttpResponseHeader\Http;
+use Ramsey\Uuid\Uuid;
 use Respect\Validation\Validator as v;
 
 
@@ -16,29 +18,53 @@ public function retrieveAll(): array{
     $Items = foodItemDal::getAll();
 
     if(count($Items) === 0){
-        foodItemDal::createDefaultItem();
-        $Items = foodItemDal::getAll();
 
+        $itemUuid = Uuid::uuid4()->toString();
+        $itemEntity = new ItemEntity();
+        $itemEntity->setItemUuid($itemUuid);
+        $itemEntity->setName('sample item');
+        $itemEntity->setPrice(100);
+        $itemEntity->setAvailable(true);
+
+        if(foodItemDal::createDefaultItem($itemEntity)){
+            $Items = foodItemDal::getAll();
+        }else{
+            $Items = [];
+        }
     }
 
-    return array_map(function (object $data){
-        unset($data['id']);
-        return $data;
-    }, $Items);
+    return $Items;
 
 }
 
-    public function retrieve(string $foodId): ?array{
+    public function retrieve(string $foodId): array{
 
 
-        if(v::uuid(version: 4)->validate($foodId)){
-            $data = foodItemDal::get($foodId);
-            unset($data['id']);
-            return $data;
+        if($foodId){
+            if (v::uuid(version: 4)->validate($foodId)) {
+               if( $data = foodItemDal::get($foodId)){
+                   if($data->getItemUuid()){
+                       return [
+                           'uuid' => $data->getItemUuid(),
+                           'name' => $data->getName(),
+                           'price' => $data->getPrice(),
+                           'available' => $data->getAvailable(),
+                       ];
+                   }
+               }
+                Http::setHeadersByCode(StatusCode::NOT_FOUND);
+
+                throw new InvalidValidationException("Invalid foodId");
+
+            }
+            Http::setHeadersByCode(StatusCode::BAD_REQUEST);
+
+            throw new InvalidValidationException("Please check your uuid");
+
         }
-        Http::setHeadersByCode(StatusCode::NOT_FOUND);
+        Http::setHeadersByCode(StatusCode::BAD_REQUEST);
 
-        throw new InvalidValidationException("invalid UUID");
+        throw new InvalidValidationException("Please check your uuid");
     }
 
 
